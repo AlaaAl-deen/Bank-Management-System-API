8565ac9bbff7463a2c1fc7386ba611835da8dcf7
AlaaAl-deen <alaaalselwi2020@gmail.com>
2026-07-16 11:51:37 +0300

    add Getaccount method in contoller

--- BankManagementSystem/Modules/Accounts/Controllers/AccountsController.cs
+++ BankManagementSystem/Modules/Accounts/Controllers/AccountsController.cs
@@ -1,6 +1,32 @@
-﻿namespace BankManagementSystem.Modules.Accounts.Controllers
+﻿using BankManagementSystem.Modules.Accounts.Responses;
+using BankManagementSystem.Modules.Accounts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankManagementSystem.Modules.Accounts.Controllers
 {
-    public class AccountsController
+    [ApiController]
+    [Route("api/accounts")]
+    public class AccountsController : ControllerBase
     {
+        private readonly AccountService _accountService;
+
+        public AccountsController()
+        {
+            _accountService = new AccountService();
+        }
+
+        [HttpGet("{customerNumber}")]
+        public ActionResult<GetAccountsResponse> GetAccounts(int customerNumber)
+        {
+            GetAccountsResponse response =
+                _accountService.GetAccounts(customerNumber);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
     }
 }
